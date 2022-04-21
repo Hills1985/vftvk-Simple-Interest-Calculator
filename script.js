4f5156545ff724755021a51f54a78d78353bf063
Hills1985 <104066810+Hills1985@users.noreply.github.com>
2022-04-21 17:59:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,21 @@ function compute()
 {
 
     var principal = document.getElementById("principal").value;
+    
+    if (principal < 0)
+    {alert("I am an alert box!");
+    }
+    else{
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
-    var amount = interest + principal/100;
+    var amount = interest + principal;
     
     document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
-}
+
+    }
+}    
 
 
 function updateRate(onchange) 
