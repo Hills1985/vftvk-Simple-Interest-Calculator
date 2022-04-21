851cb85a368897170d19a0563714b233c6b04f2a
Hills1985 <104066810+Hills1985@users.noreply.github.com>
2022-04-21 17:10:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -19,5 +19,5 @@ function updateRate(onchange)
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
     }
-    p = document.getElementById("principal").value;
+
 }
