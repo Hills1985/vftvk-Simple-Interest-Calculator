247a0706b1a41ede92175fd16b5086f5220c11bd
Hills1985 <104066810+Hills1985@users.noreply.github.com>
2022-04-21 17:15:25 +0800

    Update script.js

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ function compute(onclick)
     var amount = interest + principal;
     
     document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
-}
 
-{
+
+
 function updateRate(onchange) 
     {
     var rateval = document.getElementById("rate").value;
