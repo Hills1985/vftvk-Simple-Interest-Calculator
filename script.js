22ea5f97dea6c321e8dc6e154875b91578915a8e
Hills1985 <104066810+Hills1985@users.noreply.github.com>
2022-04-21 16:26:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 
 
-function compute()
-{
-    
-  
-    var principal = document.getElementById("principal").value;  
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
+function compute(){
+    var principal = parseFloat(document.getElementById("principal").value);  
+    var rate = parseFloat(document.getElementById("rate").value);
+    var years = parseInt(document.getElementById("years").value);
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
+    var maturitydate = new Date().getFullYear() + years;
     
     
-    document.getElementById("result").innerHTML=   "If you deposit "+principal+",\<br\>
-                                                     at an interest rate of "+rate+"%\<br\>
-                                                     You will receive an amount of "+amount+",\<br\>
-                                                     in the year "+year+"\<br\>"    
+    document.getElementById("result").innerHTML=   "If you deposit <mark>" + principal + "</mark>,\<br\>
+                                                     at an interest rate of <mark>" + rate + "</mark>%,\<br\>
+                                                     You will receive an amount of <mark> "+ interest +" </mark>,\<br\>
+                                                     in the year "+maturitydate+"\<br\>"    
 }
 
 
